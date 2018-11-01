c008c1086ab4b533297446f798d3adee550bd649
Juan Gabriel <juangabriel@frogames.es>
2018-11-01 20:17:52 +0100

    Clase 19

---
+++ Assets/Scripts/Damage.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum DamageType{
+    player,
+    enemy
+}
+
+
+public class Damage : MonoBehaviour {
+
+    public DamageType type = DamageType.enemy;
+    public float damage = 10.0f;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.GetComponent<Health>()!= null){
+            if(other.GetComponent<Health>().type != type){
+                //El tipo de daño que hacemos es diferente
+                //de a quien se lo hemos hecho
+                float currentDamage = damage;
+
+                if(other.GetComponent<Weapons>()!=null){
+                    //El otro collider lleva armas y escudos
+                    if(other.GetComponent<Weapons>().shieldActive){
+                        //El otro collider tiene el escudo activado
+                        currentDamage /= 5;
+                    }
+                }
+
+                other.GetComponent<Health>().HealthPoints -= currentDamage;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour {
+
+    public DamageType type = DamageType.enemy;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+    public float HealthPoints{
+
+        get{
+            return healthPoints;
+        }
+
+        set{
+            healthPoints = value;
+
+            if(healthPoints <= 0){
+                //TODO: gestionar la muerte del personaje / enemigo
+            }
+        }
+    }
+
+    [SerializeField]
+    private float healthPoints = 100.0f;
+
+
+
+
+}
